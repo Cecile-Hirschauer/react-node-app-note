80163dde70504a73c865a3f2a219032fbd1d9c2e
cecile hirschauer <cecile.h.75@gmail.com>
2023-10-03 15:28:28 +0200

    Add and edit notes on the UI usig form

--- note-app-ui/src/App.tsx
+++ note-app-ui/src/App.tsx
@@ -1,26 +1,69 @@
 import './App.css';
+import React, {useState} from "react";
+
+export type Note = {
+    id: number;
+    title: string;
+    content: string;
+};
 
 const App = () => {
+    const [notes, setNotes] = useState<Note[]>([]);
+
+    const [title, setTitle] = useState('');
+    const [content, setContent] = useState('');
+
+    const handleSubmit = (e: React.FormEvent) => {
+        e.preventDefault();
+        console.log("title", title);
+        console.log("content", content);
+
+        const newNote:Note = {
+            id: notes.length + 1,
+            title: title,
+            content: content
+        }
+
+        setNotes([...notes, newNote]);
+        setTitle('');
+        setContent('');
+    }
+
     return (
         <div className="app-container">
-            <form className="note-form">
-                <input type="text" placeholder="Title"/>
+            <form className="note-form"
+                  onSubmit={(e) => handleSubmit(e)}
+            >
+                <input
+                    type="text"
+                    placeholder="Title"
+                    value={title}
+                    onChange={(e) => setTitle(e.target.value)}
+                />
                 <textarea
+                    value={content}
+                    onChange={(e) => setContent(e.target.value)}
                     placeholder="Content"
                     rows={10}
                     required
                 >
             </textarea>
-                <button type={"submit"}>Add Note</button>
+                <button type={"submit"}>
+                    Add Note
+                </button>
             </form>
             <div className="notes-grid">
-                <div className="note-item">
-                    <div className="notes-header">
-                        <button>x</button>
-                    </div>
-                    <h2>Note Title</h2>
-                    <p>Note Content</p>
-                </div>
+                {
+                    notes.map((note) => (
+                        <div className="note-item" key={note.id}>
+                            <div className="notes-header">
+                                <button>x</button>
+                            </div>
+                            <h2>{note.title}</h2>
+                            <p>{note.content}</p>
+                        </div>
+                    ))
+                }
             </div>
         </div>
     );
